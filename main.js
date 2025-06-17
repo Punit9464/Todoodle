9f64c28cf582a0a981f44646e70d7f28f9b00eda
Punit9464 <punitkumar8728@gmail.com>
2025-06-17 17:13:13 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        height: 1200,
+        width: 800,
+    });
+
+    win.loadFile('home.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
